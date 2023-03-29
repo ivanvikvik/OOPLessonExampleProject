aa97a6046efa9ca32c39619a80ba6db02e6bd022
ivanvikvik <ivanvikvik@gmail.com>
2023-03-29 18:28:34 +0300

    Add Encapsulation in Student class (скрытие реализации в классе Student)

--- OOPLessonExampleProject/main.cpp
+++ OOPLessonExampleProject/main.cpp
@@ -1,15 +1,13 @@
 #include "student.h"
 
-Student get_best_student(Student st1, Student st2, Student st3) {
-	return st1.avg_mark > st2.avg_mark && st1.avg_mark > st3.avg_mark ? st1 
-		: (st2.avg_mark > st3.avg_mark ? st2 : st3);
-}
-
 int main() {
 	Student st;
 	Student st1("Ivan", "Ivanov", 14, 10);
 	Student st2("Genrih", "Gurshtinovich");
 	Student st3(14);
+
+	int age = st.get_age();
+	st.set_age(-10);
 
 	cout << st.convert_to_string() << endl;
 	
