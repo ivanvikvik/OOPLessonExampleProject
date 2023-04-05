e03dc04000a0be9049a24ce855e9f9d876b18574
ivanvikvik <ivanvikvik@gmail.com>
2023-04-05 18:47:55 +0300

    Add interface of Group class добавление интерфейсной части класса

---
+++ OOPLessonExampleProject/group.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "main.h"
+#include "student.h"
+
+#define DEFAULT_SIZE 10
+
+class Group {
+private:
+	Student list[DEFAULT_SIZE];
+	int size;
+	string name;
+
+public:
+	Group();
+	Group(Student* ls, int sz, string nm);
+	Group(string nm);
+	~Group();
+
+	void add(Student student);
+	void remove(Student student);
+	void remove(int index);
+	Student get(int index);
+	int size();
+	string convert_to_string();
+
+
+};
